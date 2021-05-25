6dfec131254bad449c3edfc2937754de56584e9f
CanekTorralba <a6519150036@utch.edu.mx>
2021-05-25 08:19:46 -0600

    Badge personalization corrected

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
   return (
     <React.Fragment>
       <Layout>
-        <Badge header_picture={"https://gaminguardian.com/wp-content/uploads/2019/01/Uzumaki.jpg"}
-        profile_picture={"https://64.media.tumblr.com/c456c44e8c3868ca28b92606f2854bd7/7399b9f78eebea69-16/s1280x1920/8b9c25c661a8f230407cbcd9fad86e77467b3f9d.png"}
-        name={"Canek Torralba"}
-        age={19}
-        city={"Chihuahua City"}
-        followers={8587}
-        likes={9364}
-        pictures={"10K"}></Badge>
+        <Badge></Badge>
       </Layout>
     </React.Fragment>
   );
--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -16,7 +16,7 @@ class NewBadge extends React.Component{
             city:"",
             followers:"",
             likes:"",
-            post:""
+            pictures:""
         }
     }
     render(){
@@ -24,7 +24,14 @@ class NewBadge extends React.Component{
             <Hero h={"15vh"}></Hero>
             <div className="container">
                 <div className="col">
-                    <Badge></Badge>
+                    <Badge header_picture={this.state.form.header_picture || "https://gaminguardian.com/wp-content/uploads/2019/01/Uzumaki.jpg"}
+                        profile_picture={this.state.form.profile_picture || "https://64.media.tumblr.com/c456c44e8c3868ca28b92606f2854bd7/7399b9f78eebea69-16/s1280x1920/8b9c25c661a8f230407cbcd9fad86e77467b3f9d.png"}
+                        name={this.state.form.name || "Canek Torralba"}
+                        age={this.state.form.age || "19"}
+                        city={this.state.form.city || "Chihuahua City"}
+                        followers={this.state.form.followers || "8587"}
+                        likes={this.state.form.likes || "9364"}
+                        pictures={this.state.form.pictures || "10K"}></Badge>
                 </div>
                 <div className="col"></div>
             </div>
