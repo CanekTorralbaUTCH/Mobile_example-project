0a037a5ad3520ecae7523da7421914a3d346169a
CanekTorralba <a6519150036@utch.edu.mx>
2021-05-25 21:36:02 -0600

    Default Badge Configuration

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -41,14 +41,14 @@ class NewBadge extends React.Component{
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
-                            <Badge header_picture={this.state.form.header_picture || "https://gaminguardian.com/wp-content/uploads/2019/01/Uzumaki.jpg"}
-                                profile_picture={this.state.form.profile_picture || "https://64.media.tumblr.com/c456c44e8c3868ca28b92606f2854bd7/7399b9f78eebea69-16/s1280x1920/8b9c25c661a8f230407cbcd9fad86e77467b3f9d.png"}
-                                name={this.state.form.name || "Canek Torralba"}
-                                age={this.state.form.age || "19"}
-                                city={this.state.form.city || "Chihuahua City"}
-                                followers={this.state.form.followers || "8587"}
-                                likes={this.state.form.likes || "9364"}
-                                pictures={this.state.form.pictures || "10K"}></Badge>
+                            <Badge header_picture={this.state.form.header_picture || "https://i2.wp.com/hipertextual.com/wp-content/uploads/2014/03/windows_xp_bliss-wide.jpg?fit=1920%2C1200&ssl=1"}
+                                profile_picture={this.state.form.profile_picture || "https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"}
+                                name={this.state.form.name || "USER NAME"}
+                                age={this.state.form.age || "AGE"}
+                                city={this.state.form.city || "Your City"}
+                                followers={this.state.form.followers || "0"}
+                                likes={this.state.form.likes || "0"}
+                                pictures={this.state.form.pictures || "0"}></Badge>
                         </div>
                         <div className="col">
                             <BadgeForm 
