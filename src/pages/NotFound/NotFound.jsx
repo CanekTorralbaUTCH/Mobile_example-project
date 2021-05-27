81036ced7ebc61b672c608df256482fb51a71adc
CanekTorralba <a6519150036@utch.edu.mx>
2021-05-26 19:56:56 -0600

    NotFound page added

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Hero from '../../components/Hero'
+import './NotFound.css'
+
+const NotFound = (props) =>{
+    return(
+        <React.Fragment>
+            <div className="Main">
+                <div className="Main_content">
+                    <h1 className="Main_title">{props.title || "Page not found"}</h1>
+                    <h3 className="Second_title">{props.title || "Page not found"}</h3>
+                </div>
+                <Hero h={"88vh"}></Hero>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default NotFound
