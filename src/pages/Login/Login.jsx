de1153c92c018fb8c6d87e0e426540e96dfbd70b
CanekTorralba <a6519150036@utch.edu.mx>
2021-05-26 18:12:59 -0600

    Login page addition

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Login.css'
+import LoginForm from '../../components/LoginForm';
+import Hero from '../../components/Hero';
+
+class Login extends React.Component{
+    state = {
+        loading : false,
+        error: null,
+        form:{
+            name:"",
+            password:"",
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Hero h={"15vh"}></Hero>
+                <div className="container">
+                    <div className="col-6">
+                        <img src="https://freedesignfile.com/upload/2019/03/Woman-reading-book-in-the-library-Stock-Photo.jpg" alt="login_image" />
+                    </div>
+                    <div className="col">
+                        <LoginForm formValues={this.state.form}></LoginForm>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Login
