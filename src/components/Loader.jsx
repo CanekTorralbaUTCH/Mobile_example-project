7d4471857f75a08e5943bc79615bad2315bfd986
CanekTorralba <a6519150036@utch.edu.mx>
2021-06-15 17:23:14 -0600

    Loader addition

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style/Loader.css"
+
+const Loader = () => {
+    return (
+        <div className="Loader">
+            <div className="lds-grid">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
